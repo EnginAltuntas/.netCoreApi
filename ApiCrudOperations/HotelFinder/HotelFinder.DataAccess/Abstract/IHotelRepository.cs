bc461038a70f5e6e55220a0640e1af9a4e91f2c4
Engin Altuntaş <61109013+EnginAltuntas@users.noreply.github.com>
2021-07-04 16:50:16 +0300

    curd operations

---
+++ ApiCrudOperations/HotelFinder/HotelFinder.DataAccess/Abstract/IHotelRepository.cs
@@ -0,0 +1,18 @@
+﻿using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelFinder.DataAccess.Abstract
+{
+    public interface IHotelRepository
+    {
+        List<Hotel> GetAllHotels();
+        Hotel GetHotelById(int id);
+
+        Hotel CreateHotel(Hotel hotel);
+        Hotel UpdateHotel(Hotel hotel);
+        void DeleteHotel(int id);
+    }
+
+}
---
+++ ApiCrudOperations/HotelFinder/HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-46M01K9\\SQLEXPRESS01;Database=HotelDb;Trusted_Connection=True;");
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+
+    }
+}
